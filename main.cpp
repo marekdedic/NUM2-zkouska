c997a6569e6ca0488dde03c5d5166700bf7ea09c
Marek Dědič <developer@dedic.eu>
2017-05-18 19:56:34 +0200

    Added Newtons method

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ using namespace std;
 
 const float PI = 3.14159265352;
 
-void write_solution(float c_1_exact, float c_2_exact, float c_1_approx, float c_2_approx)
+void write_solution(float c_1_exact, float c_2_exact, float c_1_halve, float c_2_halve, float c_1_newton, float c_2_newton)
 {
 	string head{R"(set terminal qt size 1000,1000 enhanced font 'Verdana,10' persist
 # Line width of the axes
@@ -16,6 +16,7 @@ set border linewidth 1.5
 # Line styles
 set style line 1 linecolor rgb '#0060ad' linetype 1 linewidth 2
 set style line 2 linecolor rgb '#dd181f' linetype 1 linewidth 2
+set style line 3 linecolor rgb '#00d100' linetype 1 linewidth 2
 # Axes label
 set xlabel 'x'
 set ylabel 'y'
@@ -29,22 +30,30 @@ c_1_exact = )"};
 c_2_exact = )"};
 
     string line2{R"(
-c_1_approx = )"};
+c_1_halve = )"};
 
     string line3{R"(
-c_2_approx = )"};
+c_2_halve = )"};
+
+	string line4{R"(
+c_1_newton = )"};
+
+	string line5{R"(
+c_2_newton = )"};
 
     string tail{R"(
 # Fuction
 exact(x) = c_1_exact * exp(4 * x) + c_2_exact * exp(-4 * x) + 0.5
-approx(x) = c_1_approx * exp(4 * x) + c_2_approx * exp(-4 * x) + 0.5
+halve(x) = c_1_halve * exp(4 * x) + c_2_halve * exp(-4 * x) + 0.5
+newton(x) = c_1_newton * exp(4 * x) + c_2_newton * exp(-4 * x) + 0.5
 # Plot
 plot exact(x) title 'exaktní řešení' with lines linestyle 1, \
-     approx(x) title 'přibližné řešení' with lines linestyle 2)"};
+     halve(x) title 'přibližné řešení' with lines linestyle 2, \
+	 newton(x) title 'Newtonova matoda' with lines linestyle 3)"};
 
 	ofstream file{"./solution.gnu"};
 
-	file << head << c_1_exact << line1 << c_2_exact << line2 << c_1_approx << line3 << c_2_approx << tail;
+	file << head << c_1_exact << line1 << c_2_exact << line2 << c_1_halve << line3 << c_2_halve << line4 << c_1_newton << line5 << c_2_newton << tail;
 }
 
 tuple<float, float> halve(float alpha, float beta, int rounds)
@@ -89,6 +98,23 @@ tuple<float, float> halve(float alpha, float beta, int rounds)
 	return make_tuple(c_1(alpha, gamma), c_2(alpha, gamma));
 }
 
+tuple<float, float> newton(float alpha, float beta, int rounds)
+{
+	function<float(float, float)> c_1{[](float a, float c)->float{return 0.5 * a + 0.125 * c - 0.25;}};
+	function<float(float, float)> c_2{[](float a, float c)->float{return 0.5 * a - 0.125 * c + 0.25;}};
+
+	function<float(float)> f{[alpha, beta, c_1, c_2](float c)->float{return c_1(alpha, c) * exp(2.0 * PI) + c_2(alpha, c) * exp(-2.0 * PI) + 0.5 - beta;}};
+	function<float(float)> f_der{[](float)->float{return 0.125 * exp(2.0 * PI) - 0.125 * exp(-2.0 * PI);}};
+
+	float gamma{static_cast<float>(rand()) / static_cast<float>(RAND_MAX/2000) - 1000};
+
+	for(int i = 0; i < rounds; ++i)
+	{
+		gamma = gamma - f(gamma) / f_der(gamma);
+	}
+	return make_tuple(c_1(alpha, gamma), c_2(alpha, gamma));
+}
+
 int main()
 {
 	float alpha;
@@ -104,7 +130,8 @@ int main()
 	float c_1_exact{((beta + 0.5f) + exp(-2.0f * PI) * (0.5f - alpha)) / (2.0f * sinh(2.0f * PI))};
 	float c_2_exact{alpha - 0.5f - c_1_exact};
 
-	tuple<float, float> c_approx = halve(alpha, beta, epsilon);
+	tuple<float, float> c_halve = halve(alpha, beta, epsilon);
+	tuple<float, float> c_newton = newton(alpha, beta, epsilon);
 
-	write_solution(c_1_exact, c_2_exact, get<0>(c_approx), get<1>(c_approx));
+	write_solution(c_1_exact, c_2_exact, get<0>(c_halve), get<1>(c_halve), get<0>(c_newton), get<1>(c_newton));
 }
